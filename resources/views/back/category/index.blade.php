b0cb09a18cfefd2028d4b7f3f0959f919ce595d5
Andra <maulanaandra203@gmail.com>
2025-02-09 12:17:17 +0700

    Categories

---
+++ resources/views/back/category/index.blade.php
@@ -0,0 +1,38 @@
+@extends('back.layout.template')
+
+@section('content')
+
+    {{-- content --}}
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">Categories</h1> 
+        </div>
+        
+        <div class="mt-3">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Slug</th>
+                        <th>Created At</th>
+                        <th>Function</th>
+                    </tr>
+                </thead>
+
+            </table>
+        </div>
+
+      </main>
+      {{-- footer --}}
+    </div>
+  </div>
+  
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+        <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="{{ asset('back/js/dashboard.js') }}"></script>
+    </body>
+  </html>
+  
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Back\CategoryController;
+use App\Http\Controllers\Back\DashboardController;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::resource('/categories', CategoryController::class);
